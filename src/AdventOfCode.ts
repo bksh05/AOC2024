1129b2f9c6b57fc16166b28ba048445666a89003
Bikash Singh <bksh05@gmail.com>
2024-12-01 18:54:21 +0530

    Running prettier

--- src/AdventOfCode.ts
+++ src/AdventOfCode.ts
@@ -2,32 +2,34 @@ import { readFileSync } from "fs";
 import path from "path";
 
 export abstract class AdventOfCode<T> {
-    // Protected property to hold the parsed data
-    protected data: T;
+  // Protected property to hold the parsed data
+  protected data: T;
 
-    // Constructor to ensure data is parsed when the class is instantiated
-    constructor() {
-        const input = readFileSync(path.join(__dirname, 'data', `${this.constructor.name}.txt`), 'utf-8')
-        console.log(path.join(__dirname, 'data', `${this.constructor.name}.txt`))
-        this.data = this.parseData(input);
-    }
+  // Constructor to ensure data is parsed when the class is instantiated
+  constructor() {
+    const input = readFileSync(
+      path.join(__dirname, "data", `${this.constructor.name}.txt`),
+      "utf-8",
+    );
+    console.log(path.join(__dirname, "data", `${this.constructor.name}.txt`));
+    this.data = this.parseData(input);
+  }
 
-    // Protected abstract methods for solving the challenge
-    protected abstract part1(): number;
-    protected abstract part2(): number;
+  // Protected abstract methods for solving the challenge
+  protected abstract part1(): number;
+  protected abstract part2(): number;
 
-    // Abstract method to parse input data
-    protected abstract parseData(input: string): T;
+  // Abstract method to parse input data
+  protected abstract parseData(input: string): T;
 
-    // Public method to run the application
-    public runApp() {
-        console.log(`Running ${this.constructor.name}`);
+  // Public method to run the application
+  public runApp() {
+    console.log(`Running ${this.constructor.name}`);
 
-        const result1 = this.part1();
-        console.log(`Result of part 1: ${result1}`);
+    const result1 = this.part1();
+    console.log(`Result of part 1: ${result1}`);
 
-        const result2 = this.part2();
-        console.log(`Result of part 2: ${result2}`);
-
-    }
+    const result2 = this.part2();
+    console.log(`Result of part 2: ${result2}`);
+  }
 }
